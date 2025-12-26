b5c2a63113f8d5d9dbc16fa953560687845bc977
Joy C <1415131925@qq.com>
2025-12-26 16:23:47 +0800

    Change base path to '/HumanEnrichment/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/humanenrichment/',
+      base: '/HumanEnrichment/',
       server: {
         port: 3000,
         host: '0.0.0.0',
